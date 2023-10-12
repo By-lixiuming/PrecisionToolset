409f818c599ea69235372fdc5614e3e71ff1eb63
白衣云杉醉 <lixiuming2019@gmail.com>
2023-10-12 23:05:14 +0800

    UI框架确认使用Handycontrol框架

---
+++ PrecisionToolset/ViewModels/TestViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecisionToolset.ViewModels
+{
+    public class TestViewModel : ObservableObject
+    {
+        private string _status = string.Empty;
+        public string Status
+        {
+            get => _status;
+            set => SetProperty(ref _status, value);
+        }
+
+        public TestViewModel() 
+        { 
+            Status = "Hello";
+        }
+    }
+}
